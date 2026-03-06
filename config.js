7a6693619e03328e12e0dcefacf6bc66979c421e
statsleuthgame <74439463+statsleuthgame@users.noreply.github.com>
2026-03-06 13:30:13 -0700

    Add files via upload

--- config.js
+++ config.js
@@ -8,11 +8,11 @@ const STATMASK_CONFIG = {
   mlb: {
     answer: "Nolan Arenado",
     clues: [
-      "This player's stats are from the 2025 MLB season.",
-      "He batted .237 with 35 home runs and 91 RBIs.",
-      "He plays third base and throws right-handed.",
-      "He competes in the National League.",
-      "He plays for the St. Louis Cardinals."
+      "During the 2025 season, he hit exactly .237.",
+      "California Native, in 2017, he became the first player in history to complete a cycle with a walk-off home run.",
+      "He spent his early career anchored at the 'Hot Corner'",
+      "He is the only infielder in MLB history to win a Gold Glove in each of his first 10 seasons.",
+      "He currently plays for the Arizona Diamondbacks."
     ],
     players: MLB_PLAYERS
   },
@@ -20,11 +20,11 @@ const STATMASK_CONFIG = {
   nba: {
     answer: "Karl-Anthony Towns",
     clues: [
-      "This player's stats are from the 2024-2025 NBA season.",
-      "He was the first overall pick in the 2015 NBA Draft.",
-      "He averaged 24.4 points per game this season.",
+      "This player's averaged 24.4 PPG in the 2024-2025 NBA season.",
+      "In 2024, he became only the seventh player in NBA history to record a 60-point game multiple times in his career.",
       "He played college basketball at Kentucky.",
-      "He plays for the New York Knicks."
+      "He was selected first round in the 2015 NBA Draft.",
+      "He currently plays for the New York Knicks."
     ],
     players: NBA_PLAYERS
   },
@@ -32,11 +32,11 @@ const STATMASK_CONFIG = {
   nfl: {
     answer: "Tee Higgins",
     clues: [
-      "This player's stats are from the 2024 NFL season.",
-      "He was drafted in the second round of the 2020 NFL Draft.",
-      "He recorded 846 receiving yards this season.",
-      "He plays in the AFC.",
-      "He plays for the Cincinnati Bengals."
+      "This player had 846 receving yards in the 2024 NFL season.",
+      "Actively plays for the same organization he was drafted by.",
+      "He finished his collegiate career tied with legends Sammy Watkins and DeAndre Hopkins for the most receiving touchdowns in school history (27)",
+      "Was in the top 5 for Receiving TD's in the 2025 NFL season.",
+      "He currently plays for the Cincinnati Bengals."
     ],
     players: NFL_PLAYERS
   }
